833430bbb3e872f48a28518b6a3185b8b8de1286
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-02-28 21:02:41 +0300

    c

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@ int main() {
     std::cout << "email" << std::endl;
     std::cout<<"Input email"<<std::endl;
     std::string mail;
+    std::string leftMail;
+    std::string rightMail;
+    int temp=0;
     std::getline(std::cin,mail);
+    for(int i=0;i<mail.length();i++){
+        if(mail[i]=='@') {
+            temp = i;
+            break;
+        }
+        else
+            std::cout<<"No"<<std::endl;
+    }
+    std::cout<<leftMail<<std::endl;
+
+
     return 0;
 }
